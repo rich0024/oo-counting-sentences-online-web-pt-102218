3de677037d32f04bf25f75f6880f85f093047613
Richard Yance <yance0022@gmail.com>
2018-11-27 21:26:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".")
   end
 
-  def question?
 
+  def question?
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
-
+    split(/[.!?]+/).count
   end
 end
